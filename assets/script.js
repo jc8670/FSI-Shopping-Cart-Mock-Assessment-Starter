adc3dfc7a44a8f5f44814112c5452579c458e43f
jc8670 <jc8670@gmail.com>
2022-08-01 20:49:35 -0700

    tidy up

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,19 @@
 let buttonIncrease = document.querySelector('#quantity-up');
 let buttonDecrease= document.querySelector('#quantity-down');
-
 let getQty = document.querySelector(".total-quantity");
 
- let numQty = 1;
- buttonIncrease.addEventListener("click",incrementCart);
- buttonDecrease.addEventListener("click",decrementCart);
+let numQty = 1;
+buttonIncrease.addEventListener("click",incrementCart);
+buttonDecrease.addEventListener("click",decrementCart);
  
 function incrementCart() {
     numQty++;
-getQty.innerHTML = `Quantity: ${numQty}`;
+    getQty.innerHTML = `Quantity: ${numQty}`;
 }
 
 function decrementCart() {
-    if (numQty===0) return 0;
-    numQty--;
-getQty.innerHTML = `Quantity: ${numQty}`;
+    if (numQty>0)
+        numQty--;
 
+    getQty.innerHTML = `Quantity: ${numQty}`;
 }
